b19dcbef41bdae950f377f2d851a00be8dd6d547
Naman Arora <namanarora11@gmail.com>
2016-11-10 01:55:53 +0530

    add music page path

--- music/views.py
+++ music/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h1> You have reached the music page!</h1>")
